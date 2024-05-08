86088a72c96b692fea058b8cd0f0d20f7514024d
Linda Nicole <96255722+Linmake@users.noreply.github.com>
2024-05-08 12:22:51 -0700

    Cambio en el tipo de formato, date-time por date

--- dto/ideaSchema.js
+++ dto/ideaSchema.js
@@ -2,18 +2,25 @@ import { Type } from "@sinclair/typebox"
 
 
 const ideaSchema = new Type.Object({
+
+  Id: Type.Number({
+    errorMessage: {
+      type: 'Id debe ser un número'
+    }
+  }),
+
   Titulo: Type.String({
     errorMessage: {
       type: 'El Titulo debe ser de tipo String'
     }
   }),
-  Fecha: Type.String({
-    format: 'date-time',
+  Fecha: Type.Optional(Type.String({
+    format: 'date',
     errorMessage: {
       type: 'La fecha debe ser un string',
       format: 'Fecha es el tipo de formato'
     }
-  }),
+  })),
   Texto: Type.String({
     errorMessage: {
       type: 'El texto debe ser de tipo String'

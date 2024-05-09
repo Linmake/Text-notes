de057c1e6d9f27f09826c340166e5a58800401d1
Linda Nicole <96255722+Linmake@users.noreply.github.com>
2024-05-09 10:10:02 -0700

    Se organizo dto y los schemas separados

---
+++ dto/validate-proyect-dto.js
@@ -0,0 +1,24 @@
+
+import {Ajv} from "ajv"
+import addFormats from "ajv-formats"
+import addErrors from "ajv-errors"
+import proyectSchema from "../Schema/proyectoSchema.js"
+
+
+//Ajv
+const ajv = new Ajv( {allErrors: true })
+addFormats(ajv, ['date']).addKeyword('kind').addKeyword('modifier')
+addErrors(ajv)
+
+const valid = ajv.compile(proyectSchema)
+
+
+//Middleware
+const validateProyectDTO = (( req, res, next ) => {
+  const isDTOValid = valid(req.body)
+  if (!isDTOValid) res.status(400).send(ajv.errorsText( valid.errors, {separator: '\n'} ))
+  
+    next()
+})
+
+export default validateProyectDTO

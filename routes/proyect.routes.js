8640d3aaf5e9112691e3bc4182debe2a49b369a8
Linda Nicole <96255722+Linmake@users.noreply.github.com>
2024-05-09 10:19:59 -0700

    add routes GET, POST, PUT in proyect router

--- routes/proyect.routes.js
+++ routes/proyect.routes.js
@@ -1,6 +1,33 @@
-
 import express from 'express'
+import proyectosDb from '../db/data-base.db.js'
+import validateProyectDTO from '../dto/validate-proyect-dto.js'
+
 
 const proyectRouter = express.Router()
 
-proyectRouter.use()
+ideaRouter.get( '/', (req, res) => {
+  res.status(200).send(proyectosDb)
+})
+
+proyectRouter.post('/create', validateProyectDTO, (req, res) => {
+  res.status(201).send(`Proyecto "${req.body.Titulo}" creado con exito`)
+  const proyecto = req.body
+  proyectosDb.push(proyecto)
+})
+
+ideaRouter.put('/edit/:id', validatePutReq, (req, res) => {
+  const contenido = req.body
+  const propiedades = Object.keys(contenido)
+  if(propiedades.length === 0) res.send('la petición está vácia')
+  let idea = ideasDb.find( idea => idea.Id == parseInt(req.params.id) )
+  if(!idea) res.send('Idea no existente')
+
+  idea.Id = req.body.Id || idea.Id
+  idea.Titulo = req.body.Titulo || idea.Titulo
+  idea.Texto = req.body.Texto || idea.Texto
+  idea.Fecha = req.body.Fecha || idea.Fecha
+
+  res.status(204).send('Editado con exito')
+})
+
+export default proyectRouter

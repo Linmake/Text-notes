d7d615dca7d5fdc76c8a71734fe8215571da8e96
Linda Nicole <96255722+Linmake@users.noreply.github.com>
2024-05-10 19:39:00 -0700

    Update routes index.js
    add rutas de idea, carpeta y proyecto

--- index.js
+++ index.js
@@ -2,10 +2,14 @@
 import Express from 'express'
 const expressApp = Express()
 import ideaRouter from './routes/idea.routes.js'
+import proyectRouter from './routes/proyect.routes.js'
+import carpetaRouter from './routes/carpeta.routes.js'
 import dotenv from 'dotenv'
 
 expressApp.use(Express.json())
 expressApp.use('/idea', ideaRouter)
+expressApp.use('/carpeta', carpetaRouter)
+expressApp.use('/proyecto', proyectRouter)
 
 
 dotenv.config()

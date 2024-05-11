92a5feb872a872e13ff74d82a48fd95b3b2ea870
Linda Nicole <96255722+Linmake@users.noreply.github.com>
2024-05-10 20:03:04 -0700

    Update proyect.routes.js
    Esta ruta esta completa y funcional solo faltaria la refactorizacion futura

--- routes/proyect.routes.js
+++ routes/proyect.routes.js
@@ -1,6 +1,45 @@
 
 import express from 'express'
+import { proyectosDb } from '../db/data-base.db.js'
+import validateProyectDTO from '../dto/validate-proyect-dto.js'
+import validatePutReq from '../middleware/proyecto/validatePutReq.js'
+
 
 const proyectRouter = express.Router()
 
-proyectRouter.use()
+proyectRouter.get( '/', (req, res) => {
+  res.status(200).send(proyectosDb)
+})
+
+proyectRouter.post('/create', validateProyectDTO, (req, res) => {
+  const proyecto = req.body
+  proyectosDb.push(proyecto)
+  res.status(204).send()
+})
+
+proyectRouter.put('/edit/:id', validatePutReq, (req, res) => {
+
+  let proyecto = proyectosDb.find( proyecto => proyecto.Id == parseInt(req.params.id) )
+  if(!proyecto) res.send(`Proyecto con id: ${req.params.id} no exite`)
+
+  proyecto.Id = req.body.Id || proyecto.Id
+  proyecto.Titulo = req.body.Titulo || proyecto.Titulo
+  proyecto.Fecha = req.body.Fecha || proyecto.Fecha
+
+  res.status(200).send(`Proyecto con el Id: "${ proyecto.Id }" editado con exito`)
+})
+
+proyectRouter.delete( '/delete/:id', (req, res) => {
+
+  const proyecto = proyectosDb.find( proyecto => proyecto.Id === parseInt(req.params.id) )
+  if(!proyecto) res.status(400).send(`Proyecto con id: ${req.params.id} no exite`)
+
+  const index = (proyectosDb.indexOf(proyecto))
+  proyectosDb.splice(index, 1)
+
+  res.status(200).send('Proyecto eliminado con exito')
+})
+
+/**Hasta ahora completamente hecho y funcional solo faltaria una refactorización*/
+
+export default proyectRouter

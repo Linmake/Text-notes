0ce925441fc9169b4e7c18fc4180b00a336a8b3d
Nicole Figueroa <96255722+Linmake@users.noreply.github.com>
2025-01-30 02:55:08 +0000

    vite config add

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+export default {
+  resolve: {
+    alias: {
+      '@context': '/src/context',
+      '@components-plantillas': '/src/components/plantillas',
+      '@components-sidebar': '/src/components/sidebar',
+      '@components-editor': '/src/components/editor',
+    }
+  }
+}

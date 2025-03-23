db63b59a31f56f6389f2afcd5cde8cb2f8bd43f2
Lin <lin.web.05@gmail.com>
2025-03-23 07:37:47 -0700

    Update Json Schema

---
+++ Schema/ProjectSchema.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+import { FolderSchema } from "./FolderSchema.js";
+const { Schema } = mongoose;
+
+const ProjectSchema = new Schema({
+  Id: {
+    type: String,
+    required: [true, 'UId es obligatorio.'],
+    validate: {
+      validator: String,
+      message: 'UId debe ser un String.'
+    }
+  },
+  Title: {
+    type: String,
+    required: [true, 'El Título es obligatorio.'],
+    trim: true
+  },
+  Date: {
+    type: String,
+    validate: {
+      validator: function (v) {
+        return v ? /\d{4}-\d{2}-\d{2}/.test(v) : true;
+      },
+      message: 'La Fecha debe ser de tipo String en formato YYYY-MM-DD.'
+    },
+    default: null
+  },
+  Folders: [FolderSchema]
+}, {
+  versionKey: false,
+  strict: true,
+  strictQuery: 'throw'
+});
+
+const Project = mongoose.model('Proyect', ProjectSchema);
+export default Project 

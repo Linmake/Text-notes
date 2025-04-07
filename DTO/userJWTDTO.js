8310f93b0a5cb91ff6328d28d094159c0e28adee
Nicole Figueroa <96255722+Linmake@users.noreply.github.com>
2025-04-07 09:52:14 -0700

    Update userJWTDTO.js

--- DTO/userJWTDTO.js
+++ DTO/userJWTDTO.js
@@ -1,1 +1,18 @@
+import { jwtVerify } from jose
 
+const userJWTDTO = async(res, req, next) => {
+
+	const {authorization} = req.header
+	!authorization & res.send(401)
+
+	try{
+	const encoder = new TextEncoder
+	const {payload} = await jwtVerify(authorization, encoder.encoder(process.env.JWT_PRIVATE_KEY))
+ 	req.Id = payload.Id
+	next()
+	}catch(err){
+		return res.status(401).send(err)
+	}
+}
+
+export default userJWTDTO

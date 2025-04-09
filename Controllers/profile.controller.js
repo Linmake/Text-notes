7f675374dce2f2f140b09a3476ffeeb63c7aec80
Lin <lin.web.05@gmail.com>
2025-04-08 22:52:12 -0700

    trying to fix token

--- Controllers/profile.controller.js
+++ Controllers/profile.controller.js
@@ -2,7 +2,10 @@ import Account from "../Schema/AccountSchema.js"
 
 const profileController = async(req, res) => {
     try{
-        const allAccounts = await Account.find({})
+        const { Id } = req
+        const account = await Account.findById(Id)
+        if(!account) return res.status(401).send("Incorrect credentials")
+        
         return res.status(200).send(allAccounts)
     }catch(err){
         return res.status(400).send(err)
--- DTO/userJWTDTO.js
+++ DTO/userJWTDTO.js
@@ -4,10 +4,14 @@ const userJWTDTO = async(res, req, next) => {
 
 	const {authorization} = req.header
 	!authorization & res.send(401)
+	const jwt = authorization.split(' ')[1]
 
 	try{
 	const encoder = new TextEncoder
-	const {payload} = await jwtVerify(authorization, encoder.encoder(process.env.JWT_PRIVATE_KEY))
+	const { payload } = await jwtVerify(
+		jwt, 
+		encoder.encoder(process.env.JWT_PRIVATE_KEY)
+	)
  	req.Id = payload.Id
 	next()
 	}catch(err){

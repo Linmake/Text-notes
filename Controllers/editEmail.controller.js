6b35c169fc43b071787a65b5f0aa3669334d8d3f
Lin <lin.web.05@gmail.com>
2025-04-13 02:56:16 -0700

    Update Edit Email

--- Controllers/editEmail.controller.js
+++ Controllers/editEmail.controller.js
@@ -1,19 +1,31 @@
 import { compare } from "bcrypt"
 import Account from "../Schema/AccountSchema.js"
+import { decodeJwt } from "jose"
 
 const editEmailController = async(req, res)=> {
     const { Password, Email } = req.body
-    const { idAccount } = req.params
-    const account = await Account.findOne({Id: idAccount})
-    if(!account) return res.status(400).send(`Account with Id:${idAccount} don't exists`)
+    const JWT = req.cookies.JWT
+    if(!JWT){
+        return res.status(401).send('Incorrect credentials')
+    }//factorization in one component
+    const {Id} = decodeJwt(JWT)
+    if(!Id){
+        return res.status(401).send('Incorrect credentials')
+    }
+    const account = await Account.findOne({Id: Id})
+    if(!account) {
+        return res.status(400).send(`Account with Id:${Id} don't exists`)
+    }
     const checkPassword = compare(Password, account.Password)
-    if(!checkPassword) return res.status(401).send("Incorrect creentials")
-    const accountByEmail = await Account.findOne({Email: Email})
-    if(accountByEmail) return res.status(400).send("Incorrect creentials")
-
-
-    await Account.findOneAndUpdate( { Id: idAccount }, {Email: Email} )
-    res.status(200).send(account)
+    if(!checkPassword) {
+        return res.status(401).send("Incorrect creentials")
+    }
+    const accountExistByEmail = await Account.findOne({Email: Email})
+    if(accountExistByEmail) {
+        return res.status(400).send("Incorrect creentials")
+    }
+    const updateEmailAccount = await Account.findOneAndUpdate( { Id: Id }, {Email: Email}, {new: true} )
+    res.status(200).send(updateEmailAccount)
 }
 
 export default editEmailController

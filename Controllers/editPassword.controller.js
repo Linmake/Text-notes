9afd0275740a78ba73d2834cb955f980b2e55caa
Lin <lin.web.05@gmail.com>
2025-04-13 14:06:29 -0700

    Edit Password Controller :p

--- Controllers/editPassword.controller.js
+++ Controllers/editPassword.controller.js
@@ -1,15 +1,22 @@
 import { compare } from "bcrypt"
 import Account from "../Schema/AccountSchema.js"
+import { decodeJwt } from "jose"
 
 const editPasswordController = async(req, res)=> {
     const { Password, NewPassword } = req.body
-    const { idAccount } = req.params
-    const account = await Account.findOne({Id: idAccount})
-    if(!account) return res.status(400).send(`Account with Id:${idAccount} don't exists`)
+    const JWT = req.cookies.JWT
+    if(!JWT){
+        return res.status(401).send(`Incorrect creentials`)
+    }
+    const { Id } = decodeJwt(JWT)
+    if(!Id) return res.status(401).send(`Incorrect creentials`)
+    
+    const account = await Account.findOne({Id: Id})
+    if(!account) return res.status(401).send(`Incorrect creentials`)
     const checkPassword = compare(Password, account.Password)
     if(!checkPassword) return res.status(401).send("Incorrect creentials")
 
-    await Account.findOneAndUpdate( { Id: idAccount }, {Password: NewPassword} )
+    await Account.findOneAndUpdate( { Id: Id }, {Password: NewPassword} )
     res.status(200).send(account)
 }
 

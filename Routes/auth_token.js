e21ea83a183be225c218f247fe8ff4859d013c32
Lin <lin.web.05@gmail.com>
2025-04-13 02:12:08 -0700

    Updated error status code

--- Routes/auth_token.js
+++ Routes/auth_token.js
@@ -4,16 +4,16 @@ const authToken = async(Id) => {
     const secret = process.env.JWT_PRIVATE_KEY;
 
     if (!Id) {
-      return res.status(401).send("Id not avaible")
+      return res.status(400).send("Id not avaible")
     }
     if (!secret) {
-      return res.status(401).send("Id not avaible")
+      return res.status(400).send("Id not avaible")
     }
     const jwtContructor = new SignJWT({ Id })
     const jwt = await jwtContructor
     .setProtectedHeader({alg: 'HS256', typ: 'jwt', })
     .setIssuedAt()
-    .setExpirationTime('7d')
+    .setExpirationTime('3d')
     .sign(new TextEncoder().encode(secret))
     return jwt
 }

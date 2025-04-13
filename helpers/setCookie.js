c89f0dd7e889a5da48e287721bb9fa78b1604707
Lin <lin.web.05@gmail.com>
2025-04-13 02:14:04 -0700

    Updated vars

--- helpers/setCookie.js
+++ helpers/setCookie.js
@@ -5,9 +5,9 @@
  * @param {any} cookieValue cookie value
  * @param {number} expDays expiration days
  */
-export const setCookie = (cookieName, cookieValue, expDays) => {
+export const setJWTCookie = (JWT, expDays) => {
     const date = new Date();
     date.setTime(date.getTime() + (expDays*24*60*60*1000));
-    const exp = `expires = ${date.toUTCString()}`;
-    document.cookie = `${cookieName} =  ${cookieValue}; ${exp}`;
+    const expDate = `expires = ${date.toUTCString()}`;
+    document.cookie = `JWT=${cookieValue}; ${expDate}`;
 }

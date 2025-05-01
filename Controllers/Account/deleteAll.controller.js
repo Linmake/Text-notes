214c82293768a586ab0e7cbe25fa497d5985b90f
Lin <lin.web.05@gmail.com>
2025-05-01 11:09:41 -0700

    created delete account router

--- Controllers/Account/deleteAll.controller.js
+++ Controllers/Account/deleteAll.controller.js
@@ -1,24 +1,27 @@
-import { compare } from "bcrypt"
 import Account from "../../Schema/AccountSchema.js"
+import { decodeJwt } from "jose"
 
 const deleteAllController = async(req, res)=>{
     const JWT = req.cookies.JWT
     if(!JWT || JWT == undefined){
         return res.status(401).send('Incorrect Credentials')
     }
-    const account = await Account.findOne({ Id: JWT.Id })
 
+    const Id = (decodeJwt(JWT)).Id
+
+    const account = await Account.findOne({ Id: Id })
+    
     if(!account) {
-        return res.status(400).send(`Account with Id:${ JWT.Id } don't exists`)
+        return res.status(400).send(`Account with Id:${ Id } don't exists`)
     }
-    const checkPassword = compare(Password, account.Password)
-    if(!checkPassword) {
-        return res.status(401).send("Incorrect creddentials")
+
+    if(account.Role !== "Admi") {
+        return res.status(401).send(`Without authority your Role is: ${account.Role}`)
     }
     await Account.deleteMany({})
     await account.save()
 
-    return res.status(200).send(`Account with Id:${JWT.Id} succesfully deleted`)
+    return res.status(200).send(`Account with Id:${Id} succesfully deleted`)
 }
 
 export default deleteAllController

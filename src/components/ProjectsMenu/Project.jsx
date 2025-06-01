f31607f631bf14dea309af664b45cf165676637b
Lin <lin.web.05@gmail.com>
2025-05-31 18:12:00 -0700

    add options menu in projects ✅◾◾◾

--- src/components/ProjectsMenu/Project.jsx
+++ src/components/ProjectsMenu/Project.jsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 import { useContext, useEffect, useRef, useState } from "react";
 import { ProjectsMenuContext } from "../../context/projectsMenuContext";
 import { positionSideContext } from "../../context/SideProv";
-import { faPenToSquare, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faEllipsis, faPenToSquare, faPortrait, faTrash, faTrashAlt, faTrashArrowUp, faTrashCanArrowUp } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
 
@@ -16,36 +16,56 @@ const Container = styled.li`
   width: 58vw;
   height: 80px;
   padding-top: 0.625rem;
-  padding-left: 1.2rem;
+  padding-left: 0.2rem;
   overflow-wrap: break-word;
   text-overflow: ellipsis;
   white-space: nowrap;
   display: flex;
   align-items: center;
   color: #000;
-  border: 1px solid grey;
-  gap: 10%;
+  gap: 15%;
 `;
 const ProjectInput = styled.input`
-  width: 10rem;
-  border: 1px solid grey;
+  width: 65%;
+  height: 98%;
+  border: none;
   cursor: pointer;
-  &:hover {
-    background-color: rgba(242, 244, 245, 0.45);
-  }
-  &:active {
-    background-color: rgba(242, 244, 245, 0.45);
-  }
+  border-radius: 0.2rem;
+  outline: none;
+`;
+const EditProjectInput = styled.input`
+  width: 65%;
+  height: 98%;
+  border: none;
+  border-radius: 0.2rem;
+  outline: none;
 `;
 const ContainerOptions = styled.div`
-  width: 5rem;
-  border: 1px solid grey;
+  width: 25%;
+  height: 100%;
   display: flex;
   align-items: center;
 `;
+const ContainerMenu = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 0.5rem;
+  justify-content: center;
+  align-items: center;
+  border-radius: 0.2rem;
+  position: relative;
+  right: 42%;
+  top: 85%;
+  padding: 0.5rem;
+  width: 5.5rem;
+  height: 100%;
+  z-index: 100;
+  background-color: white;
+  border: 1px solid grey;
+`;
 const DeleteIcon = styled(FontAwesomeIcon)`
   width: 1.5rem;
-  color: black;
+  color:rgb(247, 93, 76);
   &:hover {
     cursor: pointer;
   }
@@ -55,7 +75,18 @@ const DeleteIcon = styled(FontAwesomeIcon)`
 `;
 const EditIcon = styled(FontAwesomeIcon)`
   width: 1.5rem;
-  color: black;
+  color:rgb(102, 196, 240);
+  &:hover {
+    cursor: pointer;
+  }
+  &:active {
+    cursor: pointer;
+  }
+`;
+const MenuIcon = styled(FontAwesomeIcon)`
+  width: 1.7rem;
+  margin-right: 0.5rem;
+  color:rgb(26, 79, 104);
   &:hover {
     cursor: pointer;
   }
@@ -71,10 +102,12 @@ const EditIcon = styled(FontAwesomeIcon)`
 const Project = ({ Title, Id }) => {
   const { projects, setProjects } = useContext(positionSideContext);
   const navigate = useNavigate();
+  const [optsMenu, setOptsMenu] = useState(false);
   const [edit, setEdit] = useState(false);
   const [newTitle, setNewTitle] = useState(null);
   const refInput = useRef(null);
   const refEditInput = useRef(null);
+  const refMenuContainer = useRef(null)
 
   const goToProject = (Id) => {
     return navigate(`/Project/${Id}`);
@@ -87,10 +120,15 @@ const Project = ({ Title, Id }) => {
     if (!status) return;
     const currentProjects = projects.filter((project) => project.Id !== Id);
     setProjects(currentProjects);
+    setOptsMenu(false)
   };
 
   const handlerEdit = (e) => {
     setEdit(true);
+    setOptsMenu(false)
+  };
+  const handlerOptsMenu = (e) => {
+    setOptsMenu(true);
   };
 
   const handlerSaveEdit = async (event) => {
@@ -107,13 +145,29 @@ const Project = ({ Title, Id }) => {
     projectsList.splice(indexOrigProject, 0, editProject)
     setProjects(projectsList)
     setEdit(false)
+    setOptsMenu(false)
   };
 
   const handlerOnBlur = (e) => {
     setEdit(false)
+    setOptsMenu(false)
   }
 
-  useEffect( () => {}, [setEdit])
+  useEffect( () => {}, [setEdit, setOptsMenu])
+useEffect(() => {
+  const handlerOutMenu = (event) => {
+    const menu = refMenuContainer.current;
+    if (menu && !menu.contains(event.target)) {
+      setOptsMenu(false);
+    }
+  };
+
+  document.addEventListener("click", handlerOutMenu, true);
+
+  return () => {
+    document.removeEventListener("click", handlerOutMenu, true);
+  };
+}, [setOptsMenu]);
 
   return (
     <Container>
@@ -126,7 +180,7 @@ const Project = ({ Title, Id }) => {
           ref={refInput}
         />
       ) : (
-        <input
+        <EditProjectInput
           autoFocus
           ref={refEditInput}
           onChange={(e) => setNewTitle(refEditInput.current.value)}
@@ -135,8 +189,14 @@ const Project = ({ Title, Id }) => {
         />
       )}
       <ContainerOptions>
-        <DeleteIcon icon={faTrash} onClick={(e) => handlerDelete(e)} />
-        <EditIcon icon={faPenToSquare} onClick={(e) => handlerEdit(e)} />
+        <MenuIcon title="Options" icon={faEllipsis} onClick={(e) => handlerOptsMenu(e)} />
+          {(optsMenu)?
+          (
+            <ContainerMenu ref={refMenuContainer}>
+              <EditIcon  title="Edit" icon={faPenToSquare} onClick={(e) => handlerEdit(e)}/>
+              <DeleteIcon  title="Delete" icon={faTrashAlt} onClick={(e) => handlerDelete(e)} />
+            </ContainerMenu>
+          ):(<></>)}
       </ContainerOptions>
     </Container>
   );

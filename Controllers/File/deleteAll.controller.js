bbf511a16952bcc48125687b15b3d09db0d4bcee
Linmake <lin.web.05@gmail.com>
2025-07-12 23:09:50 -0700

    added options in the updateMany funct

--- Controllers/File/deleteAll.controller.js
+++ Controllers/File/deleteAll.controller.js
@@ -4,8 +4,8 @@ import Folder from "../../Schema/FolderSchema.js"
 const deleteAllFilesController = async (req, res) => {
   try {
     await File.deleteMany({})
-    await Folder.updateMany({ Files: [] })
-    res.status(200)
+    await Folder.updateMany({}, { Files: [] })
+    return res.status(200).send("All files deleted successfully")
   } catch (error) {
     res.status(500).send(`Server Error: ${error.message}`)
   }

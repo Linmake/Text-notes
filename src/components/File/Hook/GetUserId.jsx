46ef338e72d9c73a449d18eaad81fd8918d9360a
Linmake <lin.web.05@gmail.com>
2025-07-24 22:36:48 -0700

    deleted local func

--- src/components/File/Hook/GetUserId.jsx
+++
@@ -1,8 +0,0 @@
-import axios from "axios"
-
-const GetUserId = async() => {
-    const { data } = await axios.get("http://localhost:4000/account/idAccount", { withCredentials: true })
-    return data
-}
-
-export default GetUserId

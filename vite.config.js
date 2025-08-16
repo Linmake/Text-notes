edf81f12b83a164d7bcbb737d30b929f7e94a6c1
Linmake <lin.web.05@gmail.com>
2025-08-16 02:26:18 -0700

    Set base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import svgr from 'vite-plugin-svgr';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/',
   plugins: [react(), svgr(),], // ✅ Aquí agregas el plugin
   resolve: {
     alias: {
